2c31aefc9efcaf771393ac6081ac1ead55bd334c
greis <tonyxhepa.greis@gmail.com>
2022-01-23 17:36:26 +0100

    6

--- resources/js/Components/FrontendHero.vue
+++ resources/js/Components/FrontendHero.vue
@@ -13,10 +13,18 @@
                     <a href="https://laraveller.com/topics"
                        class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-large text-lg">Start</a>
                     <a href="https://youtube.com/channel/UCfBCNl3xbAC2wbbjA_9vaYg"
-                       class="ml-4 inline-flex text-gray-700 bg-red-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-400 rounded-large text-lg"
+                       class="ml-4 inline-flex space-x-1 justify-content-center text-gray-700 bg-red-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-400 rounded-large text-lg"
                        rel="noopener noreferrer"
                        target="_blank"
-                    >Youtube</a>
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"/>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+                        <span>Youtube</span>
+                    </a>
                 </div>
             </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,15 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123751041-8"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-123751041-8');
+        </script>
 
         <!-- Scripts -->
         @routes
