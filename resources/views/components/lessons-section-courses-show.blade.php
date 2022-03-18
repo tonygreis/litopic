29d8ecc7bfa6b0d04461681a1cab40e61321dc7b
laraveller <laraveller2021@gmail.com>
2022-03-18 09:58:29 +0100

    test16

--- resources/views/components/lessons-section-courses-show.blade.php
+++ resources/views/components/lessons-section-courses-show.blade.php
@@ -8,7 +8,7 @@
     <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
         @foreach ($lessons as $lesson)
             <div>
-                <a href="{{ route('frontend.lessons.show', [$lesson->serie->slug, $lesson->slug]) }}"
+                <a href="{{ route('frontend.lessons.show', [$lesson->course->slug, $lesson->slug]) }}"
                     class="group flex overflow-hidden space-x-4 items-center bg-white dark:bg-slate-800 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-large p-2">
                     <div class="w-32 h-32 rounded-large overflow-hidden shrink-0">
                         <lazy-image url="{{ $lesson->thumbnail_url }}" alt="{{ $lesson->title }}" />
@@ -23,7 +23,7 @@
             </div>
         @endforeach
     </div>
-        <div class="m-2 p-2 flex dark:bg-slate-700 dark:text-slate-300">
-            {{ $lessons->links() }}
-        </div>
+    <div class="m-2 p-2 flex dark:bg-slate-700 dark:text-slate-300">
+        {{ $lessons->links() }}
+    </div>
 </div>
