a644d440967f223cc89584fe7928d82891c20295
personal <tonyxhepa.greis@gmail.com>
2022-03-23 10:05:27 -0700

    mon

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -5,27 +5,22 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use App\Models\Lesson;
-use App\Models\Post;
-use App\Models\Serie;
 use App\Models\Topic;
 use Artesaos\SEOTools\Facades\JsonLd;
 use Artesaos\SEOTools\Facades\OpenGraph;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\TwitterCard;
-use Illuminate\Support\Facades\Request;
-use Inertia\Inertia;
-use Wink\WinkPost;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         SEOMeta::setTitle('Laravel Tutorials for beginners | Laraveller');
-        SEOMeta::setDescription('Learn Laravel from beginner to advanced. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
+        SEOMeta::setDescription('Laravel Tutorials for beginners. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
         SEOMeta::setCanonical(url()->current());
         SEOMeta::addKeyword(['Laravel', 'tutorlais', 'vuejs', 'livewire']);
 
-        OpenGraph::setDescription('Learn Laravel from beginner to advanced. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
+        OpenGraph::setDescription('Laravel Tutorials for beginners. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
         OpenGraph::setTitle('Laravel Tutorials for beginners | Laraveller');
         OpenGraph::setUrl(url()->current());
         OpenGraph::addProperty('type', 'articles');
@@ -34,7 +29,7 @@ class WelcomeController extends Controller
         TwitterCard::setSite('@Laravellercom');
 
         JsonLd::setTitle('Laravel Tutorials for beginners | Laraveller');
-        JsonLd::setDescription('Learn Laravel from beginner to advanced. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
+        JsonLd::setDescription('Laravel Tutorials for beginners. Laravel Livewire tutorials, Vuejs, Inertiajs and more. Laravel free courses.');
         JsonLd::addImage(asset('images/logo.svg'));
 
         $topics = Topic::orderBy('created_at', 'desc')->take(8)->get();
