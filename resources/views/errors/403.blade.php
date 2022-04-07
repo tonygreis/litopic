2ae3cd985b35f2b3a1b5b1d2858acb022297b47a
personal <tonyxhepa.greis@gmail.com>
2022-04-07 10:23:10 +0200

    add errors

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,35 @@
+<x-main-layout>
+    <div class="max-w-7xl mx-auto">
+        <div class="flex items-center justify-center w-screen h-screen">
+            <div class="px-4 lg:py-12">
+                <div class="lg:gap-4 lg:flex">
+                    <div
+                        class="flex flex-col items-center justify-center md:py-24 lg:py-32"
+                    >
+                        <h1 class="font-bold text-blue-600 text-9xl">404</h1>
+                        <p
+                            class="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl"
+                        >
+                            <span class="text-red-500">Oops!</span> That's an error.
+                        </p>
+                        <p class="mb-8 text-center text-gray-500 md:text-lg">
+                            We're sorry, but you do not have access to this page.
+                        </p>
+                        <a
+                            href="/"
+                            class="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
+                        >Go home</a
+                        >
+                    </div>
+                    <div class="mt-4">
+                        <img
+                            src="https://laraveller.com/img/hero.svg"
+                            alt="img"
+                            class="object-cover w-full h-full"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-main-layout>
